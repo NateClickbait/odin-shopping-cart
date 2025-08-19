6f6f4812e23837bed4b8a758dd824b1658b53d7b
NateClickbait <andrewtruong40024@gmail.com>
2025-08-19 13:33:04 -0500

    fetched products and added loading component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from '../styles/App.module.css'
 
 import Header from './Header';
+import { Outlet } from 'react-router-dom';
+import ErrorPage from './ErrorPage';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+        console.log(data);
+      }
+      catch {
+        //display errorPage
+        return <ErrorPage />;
+      }
+    }
+
+    getProducts();
+  }, [])
+
   return (
     <>
       <Header />
-      <main className={styles.main}></main>
+      <Outlet />
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
 import styles from "../styles/Home.module.css";
+import Loading from "./Loading";
 
 function Home() {
-
+  return (
+    <div className={styles.home}>
+      <Loading />
+    </div>
+  );
 }
 
 export default Home;
