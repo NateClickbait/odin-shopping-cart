72a04a2f3cda5453de3206a8b3a20f74ef4360f5
NateClickbait <andrewtruong40024@gmail.com>
2025-08-19 12:36:00 -0500

    added error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,7 +1,14 @@
 import styles from "../styles/ErrorPage.module.css";
 
 function ErrorPage() {
-
+	return (
+		<div className={styles.error}>
+			<h1 className={styles.unhappy}>:(</h1>
+			<p className={styles.errorMessage}>This website ran into a problem and needs a restart. 
+			   This Page most likely does not exist within this website</p>
+			<p className={styles.errorMessage}>ERROR</p>
+		</div>
+	);
 }
 
 export default ErrorPage;
